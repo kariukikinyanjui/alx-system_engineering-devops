f606dc1beec68afdaedca22ea3ea8fc218574566
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-12-21 16:23:51 +0000

    Client config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 # Configuration file with (w/Puppet)
 
-file { 'etc/ssh/ssh_config':
-        ensure => present,
-
-content =>"
-        #SSH client configuration
-        host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
-}
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => 'PasswordAuthentication no\nIdentityFile ~/.ssh/school\n',
+  }
