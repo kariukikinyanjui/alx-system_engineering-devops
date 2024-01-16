f5a1006b62e7133de2f7449e0613fa6f3bccec72
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-01-16 21:02:26 +0000

    [Feature]: function that queries the Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import requests
+"""
+A function that queries the Reddit API and returns the number of
+subscribers for a given subreddit
+"""
+
+
+def number_of_subscribers(subreddit):
+    """
+    function takes a subreddit name as an argument and constructs the URL
+    for subreddit's information in JSON format
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    headers = {'User-Agent': 'CustomUserAgent'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return 0
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        print("{:d}".format(number_of_subscribers(subreddit)))
