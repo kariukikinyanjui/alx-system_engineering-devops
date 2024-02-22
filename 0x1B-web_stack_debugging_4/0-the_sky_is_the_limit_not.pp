51c70269a52d5b235755ace51a0b3aab8710393a
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-02-22 06:00:29 +0000

    [feat]: Increased traffic that the web server can handle

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Handle Nginx server traffic
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  # Modify the value of ULIMIT and specifiy the path for the sed command
+  command => '/bin/sed -i "s/25/4096/" /etc/default/nginx',
+  path    => '/user/local/bin/:/bin/',
+}
+
+# Restart web server
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
