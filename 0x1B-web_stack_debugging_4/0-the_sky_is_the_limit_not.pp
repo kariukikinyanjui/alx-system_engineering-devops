e9cab8d9759c96c0d2cd25eeeb204c687ec4e07e
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-02-23 12:57:41 +0000

    [feat]: updated web server to handle more traffic

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,21 +4,6 @@ exec { 'fix--for-nginx':
   # Modify the value of ULIMIT and specifiy the path for the sed command
   command => '/bin/sed -i "s/25/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
-  before  => Exec['nginx-restart'],
-}
-
-# Configure Nginx settings for handling 1000 requests with 100 at a time
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  content => "worker_processes 4;\nevents {\n\tworker_connects 100;\n}",
-  notify  => Exec['nginx-reload'],
-}
-
-# Reload Nginx configuration
-exec { 'nginx-reload':
-  command     => '/etc/init.d/nginx reload',
-  path        => '/etc/init.d/',
-  refreshonly => true,
 }
 
 # Restart web server
